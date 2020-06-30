688a457ed6147f3450dd815c8aa4ca63c9356d4d
John Bieling <john.bieling@gmx.de>
2020-06-30 18:13:12 +0200

    actually do something

--- examples/MailExtensions/WindowListener/content/addressbook.js
+++ examples/MailExtensions/WindowListener/content/addressbook.js
@@ -1,11 +1,30 @@
 var { myModule } = ChromeUtils.import(this.extension.rootURI.resolve("content/modules/myModule.jsm"));
 
-function onLoad(window, wasAlreadyOpen) {
+const restartTime = "now";
+
+function restart(e) {
+	let document = e.target.ownerDocument;
+	let window = document.defaultView;
+	window.alert("I (" + this.extension.id + ") should restart: " + restartTime);
+}
+
+function onLoad(window, wasAlreadyOpen) {	
 	myModule.incValue();
 	console.log("onLoad from the ADDRBOOK script has been called by <"+this.extension.id+">");
 	console.log("onLoad() has been called for already open window: " + wasAlreadyOpen);
 	console.log("onLoad() for: " + window.location.href);
 	console.log("onLoad() sees myModule value : " + myModule.getValue());
+
+	// create a UI element via JavaScript (copied from Geoffs restart example)
+	let myItem = window.document.createXULElement("menuitem");
+	myItem.id = "myRestartEntry";
+	myItem.setAttribute("label", this.extension.localeData.localizeMessage("restartLabel"));
+	myItem.setAttribute("accesskey", "R");
+	myItem.addEventListener("command", restart.bind(this));
+	
+	// add the new UI element
+	let refItem = window.document.getElementById("menu_close");
+	refItem.parentNode.insertBefore(myItem, refItem);
 }
 
 function onUnload(window, isAddOnShutDown) {
@@ -14,4 +33,8 @@ function onUnload(window, isAddOnShutDown) {
 	console.log("onUnload() has been called because of global add-on shutdown: " + isAddOnShutDown);
 	console.log("onUnload() for: " + window.location.href);
 	console.log("onUnload() sees myModule value : " + myModule.getValue());
+	
+	// remove any added elements
+	let me = window.document.getElementById("myRestartEntry");
+	me.remove();	
 }
