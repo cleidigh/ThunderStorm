2ef4bb951514d1585ea2798465c494e0fdf35ba5
John Bieling <john.bieling@gmx.de>
2020-07-04 21:54:12 +0200

    that -> self

--- examples/MailExtensions/WindowListener/content/api/WindowListener/implementation.js
+++ examples/MailExtensions/WindowListener/content/api/WindowListener/implementation.js
@@ -15,7 +15,7 @@ var WindowListener = class extends ExtensionCommon.ExtensionAPI {
     
     const aomStartup = Cc["@mozilla.org/addons/addon-manager-startup;1"].getService(Ci.amIAddonManagerStartup);
 
-    let that = this;
+    let self = this;
 
     return {
       WindowListener: {
@@ -26,66 +26,66 @@ var WindowListener = class extends ExtensionCommon.ExtensionAPI {
             null,
             context.extension.rootURI
           );
-          that.chromeHandle = aomStartup.registerChrome(manifestURI, chromeData);          
+          self.chromeHandle = aomStartup.registerChrome(manifestURI, chromeData);          
         },
 
         registerWindow(windowHref, jsFile) {
-          if (!that.registeredWindows.hasOwnProperty(windowHref)) {
+          if (!self.registeredWindows.hasOwnProperty(windowHref)) {
             // path to JS file can either be chrome:// URL or a relative URL
             let path = jsFile.startsWith("chrome://") 
               ? jsFile 
               : context.extension.rootURI.resolve(jsFile)
-            that.registeredWindows[windowHref] = path;
+            self.registeredWindows[windowHref] = path;
           } else {
             console.error("Window <" +windowHref + "> has already been registered");
           }
         },
 
         registerShutdownScript(aPath) {
-          that.pathToShutdownScript = aPath.startsWith("chrome://") 
+          self.pathToShutdownScript = aPath.startsWith("chrome://") 
             ? aPath
             : context.extension.rootURI.resolve(aPath);
         },
         
         startListening(namespace) {
-          that.namespace = namespace;
-          let urls = Object.keys(that.registeredWindows);
+          self.namespace = namespace;
+          let urls = Object.keys(self.registeredWindows);
           if (urls.length > 0) {
             // Before registering the window listener, check which windows are already open
-            that.openWindows = [];
+            self.openWindows = [];
             for (let window of Services.wm.getEnumerator(null)) {
-              that.openWindows.push(window);
+              self.openWindows.push(window);
             }
             
             
             // Register window listener for all pre-registered windows
             ExtensionSupport.registerWindowListener("injectListener", {
-              chromeURLs: Object.keys(that.registeredWindows),
+              chromeURLs: Object.keys(self.registeredWindows),
               onLoadWindow(window) {
                 try {
                   // Create add-on specific namespace
                   window[namespace] = {};
                   // Make extension object available in loaded JavaScript
-                  window[namespace].extension = that.extension;
+                  window[namespace].extension = self.extension;
                   // Add messenger obj
-                  window[namespace].messenger = Array.from(that.extension.views).find(
+                  window[namespace].messenger = Array.from(self.extension.views).find(
                     view => view.viewType === "background").xulBrowser.contentWindow
                     .wrappedJSObject.browser;                  
                   // Load script into add-on specific namespace
-                  Services.scriptloader.loadSubScript(that.registeredWindows[window.location.href], window[namespace], "UTF-8");
+                  Services.scriptloader.loadSubScript(self.registeredWindows[window.location.href], window[namespace], "UTF-8");
                   // Call onLoad(window, wasAlreadyOpen)
-                  window[namespace].onLoad(window, that.openWindows.includes(window));
+                  window[namespace].onLoad(window, self.openWindows.includes(window));
                 } catch (e) {
                   Components.utils.reportError(e)
                 }
               },
               onUnloadWindow(window) {
                 //  Remove this window from the list of open windows
-                that.openWindows = that.openWindows.filter(e => (e != window));    
+                self.openWindows = self.openWindows.filter(e => (e != window));    
                 
                 try {
                   // Call onUnload()
-                  window[that.namespace].onUnload(window, false);
+                  window[self.namespace].onUnload(window, false);
                 } catch (e) {
                   Components.utils.reportError(e)
                 }
